a8d382058b967d6483809d13536333eba082c90d
toothsy <c.j.atharva@gmail.com>
2021-04-12 20:39:11 +0530

    build files

---
+++ build/precache-manifest.f23b9d49e6d33cd423ead1f6ced40a0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae58e204b16f1dbf266bd872a7fe84dc",
+    "url": "/colorGame/index.html"
+  },
+  {
+    "revision": "5e2d9395127e0862ee5f",
+    "url": "/colorGame/static/css/main.b1bc086c.chunk.css"
+  },
+  {
+    "revision": "ab138c6fd56cd3213397",
+    "url": "/colorGame/static/js/2.9755bea3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colorGame/static/js/2.9755bea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2d9395127e0862ee5f",
+    "url": "/colorGame/static/js/main.1d7f9579.chunk.js"
+  },
+  {
+    "revision": "66c7894a5abd9b30ab7a",
+    "url": "/colorGame/static/js/runtime-main.1015305e.js"
+  }
+]);
